23f5fab57aa7810c398e858b78fd6ce520db59ec
Tugdual Grall <tgrall@github.com>
2021-11-24 15:53:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
-			fmt.Fprintln(w, "{ \"msg\" : \"Hello world\", \"version\" : \"v1.0.17\" }")
+			fmt.Fprintln(w, "{ \"msg\" : \"Hello world\", \"version\" : \"v1.0.20\" }")
 		})
 
 	log.Fatalf(

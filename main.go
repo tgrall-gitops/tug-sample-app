d705efe83455db09eb4ba06103e8bccd81c1e6f4
Tugdual Grall <tgrall@github.com>
2021-11-30 14:11:19 +0100

    add template and ns in page

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
+type Page struct {
+	Title     string
+	Version   string
+	Namespace string
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+
+	namespace := os.Getenv("KUBE_NS")
+
+	p := Page{"Go Simple Application", "1.0.0", namespace}
+	t := template.New("Template")
+	t = template.Must(t.ParseFiles("static/page.tmpl"))
+	err := t.ExecuteTemplate(w, "layout", p)
+	if err != nil {
+		log.Fatalf("Template execution: %s", err)
+	}
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
